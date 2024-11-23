35e23044562d94e777359124a41e249f97ba4380
Daniel Johnson <danj256@gmail.com>
2024-11-22 21:21:56 -0700

    Add random seed to q3 for reproducibility

--- hw5_3.py
+++ hw5_3.py
@@ -3,8 +3,9 @@ from collections import Counter
 
 POPULATION = 1000000
 
+np.random.seed(4321)
 
-def experiment():
+def experiment(num_samples):
     ones = np.ones(int(.35 * POPULATION))
     neg_ones = np.full(int(.4 * POPULATION), -1)
     zeros = np.zeros(int(.25 * POPULATION))
@@ -15,12 +16,15 @@ def experiment():
     sum_neg_one = 0
 
     for i in range(200):
-        samp = np.random.choice(all_votes, 200)
+        samp = np.random.choice(all_votes, num_samples, replace=False)
         counts = Counter(samp)
+        most_common = counts.most_common(1)[0]
         if counts.most_common(1)[0][0] == -1:
+        # if most_common[0] == -1 and most_common[1] >= NUM_SAMPLES / 2:
             sum_neg_one += 1
 
     return float(sum_neg_one) / 200
 
-
-print(experiment())
+print(experiment(10))
+print(experiment(120))
+print(experiment(250))
